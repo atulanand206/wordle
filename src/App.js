c1e89eaa543853b9ce9faed7158a292ba71de00f
Atul Anand <atulanand206@gmail.com>
2022-02-25 03:09:05 +0530

    Moved Wordle at end

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
       <h1>War of Wordles</h1>
       <div className="Games">
         <iframe title="Worldle" width="400" height="800" src="https://worldle.teuteuf.fr/"/>
-        <iframe title="Wordle" width="400" height="800" src="https://www.nytimes.com/games/wordle/index.html"/>
         <iframe title="Quordle" width="400" height="800" src="https://www.quordle.com/#/"/>
         <iframe title="Nerdle" width="400" height="800" src="https://nerdlegame.com/"/>
+        <iframe title="Wordle" width="400" height="800" src="https://www.nytimes.com/games/wordle/"/>
       </div>
     </div>
   );
