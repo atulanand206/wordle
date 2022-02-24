6f6f9719fab30d640867107be2eee013d5d09ce2
Atul Anand <atulanand206@gmail.com>
2022-02-25 03:20:57 +0530

    Added Absurdle

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <iframe title="Worldle" width="400" height="800" src="https://worldle.teuteuf.fr/"/>
         <iframe title="Quordle" width="400" height="800" src="https://www.quordle.com/#/"/>
         <iframe title="Nerdle" width="400" height="800" src="https://nerdlegame.com/"/>
-        <iframe title="Wordle" width="400" height="800" src="https://www.nytimes.com/games/wordle/"/>
+        <iframe title="Absurdle" width="400" height="800" src="https://qntm.org/files/absurdle/absurdle.html"/>
       </div>
     </div>
   );
