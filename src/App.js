b1f3601a584515b280e80acffc9a2b43cfddc941
Atul Anand <atulanand206@gmail.com>
2022-02-25 03:31:53 +0530

    Moved Worldle at the end.

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
     <div className="App">
       <h1>War of Wordles</h1>
       <div className="Games">
-        <iframe title="Worldle" width="400" height="800" src="https://worldle.teuteuf.fr/"/>
         <iframe title="Quordle" width="400" height="800" src="https://www.quordle.com/#/"/>
         <iframe title="Nerdle" width="400" height="800" src="https://nerdlegame.com/"/>
         <iframe title="Absurdle" width="400" height="800" src="https://qntm.org/files/absurdle/absurdle.html"/>
+        <iframe title="Worldle" width="400" height="800" src="https://worldle.teuteuf.fr/"/>
       </div>
     </div>
   );
